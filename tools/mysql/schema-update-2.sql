b37aa6c34ea56ec2aada9713f48d503135ccb310
Olivier Mignault <olivier@mignault.ca>
2020-04-16 16:49:12 -0400

    add changes

--- tools/mysql/schema-update-2.sql
+++ tools/mysql/schema-update-2.sql
@@ -2,6 +2,35 @@
 -- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT   
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+-- add missing commits from https://github.com/pinterest/teletraan/pull/491/files#diff-380d85d61edaf8019bd4a4cd16a48c36
+
+ALTER TABLE environs ADD COLUMN schedule_id VARCHAR(22);
+
+CREATE TABLE IF NOT EXISTS host_tags (
+    host_id        VARCHAR(64)         NOT NULL,
+    env_id         VARCHAR(22)         NOT NULL,
+    tag_name       VARCHAR(64)         NOT NULL,
+    tag_value      VARCHAR(256),
+    create_date     BIGINT              NOT NULL,
+    PRIMARY KEY    (host_id, env_id, tag_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE INDEX host_tags_env_idx ON host_tags (env_id);
+CREATE INDEX host_tags_env_host_idx ON host_tags (env_id, host_id);
+
+CREATE TABLE IF NOT EXISTS deploy_constraints (
+  constraint_id     VARCHAR(22)         NOT NULL,
+  constraint_key    VARCHAR(64)         NOT NULL,
+  max_parallel      BIGINT              NOT NULL,
+  state             VARCHAR(32)         NOT NULL,
+  start_date        BIGINT              NOT NULL,
+  last_update       BIGINT,
+  PRIMARY KEY   (constraint_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE environs ADD COLUMN deploy_constraint_id VARCHAR(22);
+
+
 CREATE INDEX agent_first_deploy_state_idx ON agents (env_id,first_deploy, deploy_stage ,state);
 
 -- make sure to update the schema version to 1
